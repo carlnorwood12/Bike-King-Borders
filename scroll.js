038aa3c91c761fa98683852534c1a602c2d0dbb2
Carl <94581214+carlnorwood12@users.noreply.github.com>
2024-06-05 12:57:32 +0100

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -9,6 +9,7 @@ gsap.to("#bannertext", {
   },
 });
 gsap.set("#h1text", { xPercent: -100, opacity: 0 });
+
 gsap.to("#h1text", {
   xPercent: 0,
   opacity: 1,
@@ -18,7 +19,10 @@ gsap.to("#h1text", {
     scrub: true,
     trigger: "#bkblogo",
     start: "top",
-    end: "bottom+=1000",
+    end: () => {
+      const extraDistance = window.innerHeight * 2;
+      return `bottom+=${extraDistance}`;
+    },
   },
 });
 gsap.set("#bkblogo", {
