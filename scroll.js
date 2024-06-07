b7c2da026a6b72f9fe69a27335de5e7a186f6675
Carl <94581214+carlnorwood12@users.noreply.github.com>
2024-06-07 16:07:50 +0100

    Add files via upload

--- scroll.js
+++ scroll.js
@@ -18,13 +18,6 @@ gsap.to(".bannerheader", {
     end: "bottom+=10",
   },
 });
-gsap.set("#bkblogo", {
-  opacity: 0,
-  duration: 10,
-});
-gsap.to("#bkblogo", {
-  opacity: 1,
-});
 gsap.set(".contact-1", { opacity: 0 });
 gsap.to(".contact-1", {
   opacity: 1,
